f999af0f1eee9b5bd708a5451be00170bb3375f1
Craig Yu <craig.yu93@gmail.com>
2025-01-03 18:43:20 -0800

    fix: restore color

--- vite.theme.config.ts
+++ vite.theme.config.ts
@@ -2,20 +2,6 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import dts from 'vite-plugin-dts';
 
-const themeHashes = {
-  global: 'XhWg9q',
-  light: 'YqfL3a',
-  dark: 'R-l9gW',
-  medium: 'rfm_fq',
-  large: '_1DrGeG',
-};
-
-// Generate hash based on theme
-const generateScopedName = (name, theme) => {
-  const hash = themeHashes[theme] || themeHashes.global; // Default to global
-  return `${hash}_${name}`;
-};
-
 export default defineConfig({
   build: {
     lib: {
@@ -34,17 +20,6 @@ export default defineConfig({
     dts({
       outDir: 'dist/theme',
       insertTypesEntry: true,
-    })
+    }),
   ],
-  css: {
-    modules: {
-      generateScopedName: (name, filename) => {
-        if (filename.includes('light')) return generateScopedName(name, 'light');
-        if (filename.includes('dark')) return generateScopedName(name, 'dark');
-        if (filename.includes('medium')) return generateScopedName(name, 'medium');
-        if (filename.includes('large')) return generateScopedName(name, 'large');
-        return generateScopedName(name, 'global');
-      },
-    },
-  },
 });
